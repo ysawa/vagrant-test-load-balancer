931fe157468886f23d0e02c03e8a304222c6e965
Y Sawa <yoshihirosawa@gmail.com>
2012-11-28 21:45:02 +0900

    fix: enable to init redis

--- puppet/modules/redis/files/install.sh
+++ puppet/modules/redis/files/install.sh
@@ -2,9 +2,14 @@
 
 cd /tmp
 
+user=redis
 redis=redis-2.6.5
 redis_file=${redis}.tar.gz
 redis_url=http://redis.googlecode.com/files/${redis_file}
+
+if ! id -u $user > /dev/null 2>&1; then
+  useradd -s /sbin/nologin $user
+fi
 
 wget $redis_url
 mv $redis $redis_file
--- puppet/modules/redis/manifests/init.pp
+++ puppet/modules/redis/manifests/init.pp
@@ -15,4 +15,65 @@ class redis {
     cwd       => '/tmp/',
     unless => '/bin/ls /usr/local/bin/redis-server', # TODO make condition more specifically
   }
+
+  $redis_directories = ['/var/run/redis', '/var/lib/redis', '/var/log/redis', '/var/lock/redis', '/usr/local/redis']
+  file { $redis_directories:
+    require => [
+      Exec["/tmp/puppet_redis_install.sh"],
+    ],
+    ensure  => 'directory',
+    mode    => '0755',
+    owner   => 'redis',
+    group   => 'redis',
+  }
+
+  exec { 'chown redis directories':
+    require => [
+      File[$redis_directories],
+    ],
+    command => '/bin/chown -R redis:redis /var/run/redis /var/lib/redis /var/log/redis /var/lock/redis; true',
+  }
+
+  file { '/etc/init.d/redis-server':
+    require => [
+      Exec['chown redis directories'],
+    ],
+    ensure  => 'file',
+    source  => 'puppet:///modules/redis/redis-server.sh',
+    replace => 'no',
+    mode    => '0777',
+    owner   => root,
+    group   => root,
+  }
+
+  file { '/etc/redis.conf':
+    require => [
+      Exec['chown redis directories'],
+    ],
+    ensure  => 'file',
+    source  => 'puppet:///modules/redis/redis.conf',
+    # replace => 'no',
+    mode    => '0644',
+    owner   => redis,
+    group   => redis,
+  }
+
+  file { '/etc/init/redis-server.conf':
+    require => [
+      File['/etc/redis.conf'],
+    ],
+    ensure  => 'file',
+    source  => 'puppet:///modules/redis/init/redis-server.conf',
+    # replace => 'no',
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+  }
+
+  exec { 'start redis-server':
+    require => [
+      File['/etc/init/redis-server.conf'],
+    ],
+    command => '/sbin/start redis-server',
+  }
 }
