d8070963b9102e4a6fa5da3b1d95e88b9eb5bf91
Y Sawa <yoshihirosawa@gmail.com>
2012-11-28 10:14:27 +0900

    add: configuration of mongod

--- puppet/modules/mongodb/manifests/init.pp
+++ puppet/modules/mongodb/manifests/init.pp
@@ -1,17 +1,35 @@
 class mongodb {
 
-  file { '/tmp/mongodb.sh':
+  file { '/tmp/puppet_mongodb_install.sh':
     ensure  => 'file',
     source  => 'puppet:///modules/mongodb/install.sh',
-    mode    => '777',
+    mode    => '0777',
     owner   => root,
     group   => root,
   }
 
-  exec { "/tmp/mongodb.sh":
+  exec { "/tmp/puppet_mongodb_install.sh":
     require => [
-      File['/tmp/mongodb.sh'],
+      File['/tmp/puppet_mongodb_install.sh'],
     ],
     cwd       => '/tmp/',
+  }
+
+  file { '/etc/mongod.conf':
+    ensure  => 'file',
+    replace => 'no',
+    source  => 'puppet:///modules/mongodb/mongod.conf',
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+  }
+
+  file { '/etc/init.d/mongod':
+    ensure  => 'file',
+    replace => 'no',
+    source  => 'puppet:///modules/mongodb/mongod.sh',
+    mode    => '0755',
+    owner   => root,
+    group   => root,
   }
 }
