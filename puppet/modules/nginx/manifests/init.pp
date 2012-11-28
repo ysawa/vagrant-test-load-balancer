b8c9b3866a2835d6a6442d72319720c4cb76ea81
Y Sawa <yoshihirosawa@gmail.com>
2012-11-28 11:07:16 +0900

    modify: naming

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -3,7 +3,7 @@ class nginx {
   package { ['apache2']: ensure => "absent" }
   package { ['libpcre3', 'libpcre3-dev']: ensure => "installed" }
 
-  file { '/tmp/nginx.sh':
+  file { '/tmp/puppet-install-nginx.sh':
     ensure  => 'file',
     source  => 'puppet:///modules/nginx/install.sh',
     mode    => '0777',
@@ -11,9 +11,16 @@ class nginx {
     group   => root,
   }
 
-  exec { "/tmp/nginx.sh":
+  file { '/etc/nginx/conf.d':
+    ensure  => 'directory',
+    mode    => '0755',
+    owner   => 'nginx',
+    group   => 'nginx',
+  }
+
+  exec { "/tmp/puppet-install-nginx.sh":
     require => [
-      File['/tmp/nginx.sh'],
+      File['/tmp/puppet-install-nginx.sh'],
     ],
     cwd       => '/tmp/',
   }
