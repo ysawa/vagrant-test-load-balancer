8d2949fe2367666859bb7b348e8fd34d2bb0b5b7
Y Sawa <yoshihirosawa@gmail.com>
2012-11-28 14:58:58 +0900

    modify: installation more efficiently

--- puppet/modules/mongodb/manifests/init.pp
+++ puppet/modules/mongodb/manifests/init.pp
@@ -13,26 +13,12 @@ class mongodb {
       File['/tmp/puppet_mongodb_install.sh'],
     ],
     cwd       => '/tmp/',
-    # unless => '/bin/ls /usr/local/bin/mongod', # TODO make condition more specifically
-  }
-
-  exec { 'apt-get update for mongodb-10gen':
-    require => [
-      Exec['/tmp/puppet_mongodb_install.sh'],
-    ],
-    command => '/usr/bin/apt-get update -y',
-  }
-
-  package { ['mongodb-10gen']:
-    require => [
-      Exec['apt-get update for mongodb-10gen'],
-    ],
-    ensure => 'installed',
+    unless => '/bin/ls /usr/bin/mongod', # TODO make condition more specifically
   }
 
   exec { 'sysv-rc-conf mongodb on':
     require => [
-      Package['mongodb-10gen'],
+      Exec["/tmp/puppet_mongodb_install.sh"],
     ],
     command => '/usr/sbin/sysv-rc-conf mongodb on',
   }
