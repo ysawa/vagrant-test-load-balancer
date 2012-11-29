1534d669e1db4eaeae608954062ad91e0821124b
Y Sawa <yoshihirosawa@gmail.com>
2012-11-29 19:00:39 +0900

    add: essentials as a module

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -1,11 +1,7 @@
 exec { 'apt-get update': command => '/usr/bin/apt-get update -y', }
 # package { 'nginx': ensure => present, require => Exec['apt-get update'], }
 
-$essentials = ["build-essential", "libxslt1.1", "libxml2", "libssl-dev", "git-core", "libffi-dev", "libsqlite3-dev", "libreadline5-dev", "vim"]
-package { $essentials:
-  ensure => "installed"
-}
-
+include essentials
 include nginx
 include mongodb
 include graphicsmagick
---
+++ puppet/modules/essentials/manifests/init.pp
@@ -0,0 +1,6 @@
+class essentials {
+  $essentials = ["build-essential", "libxslt1.1", "libxml2", "libssl-dev", "git-core", "libffi-dev", "libsqlite3-dev", "libreadline5-dev", "vim"]
+  package { $essentials:
+    ensure => "installed"
+  }
+}
