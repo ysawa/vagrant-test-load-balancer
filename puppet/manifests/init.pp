1c2e53a117326c0415eaf6e511ff917eeef7d8d2
Y Sawa <yoshihirosawa@gmail.com>
2012-12-03 16:56:05 +0900

    refactor: nodes

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,6 +1,7 @@
 
 node default {
   exec { 'apt-get update': command => '/usr/bin/apt-get update -y', }
+  include hosts
   include ppa
   include essentials
   include php5
@@ -9,7 +10,6 @@ node default {
   include redis
   include mongodb
   include mongodb::replication
-  include hosts
   include editors
   include nagios
   # include ganglia
@@ -20,11 +20,5 @@ node m0 inherits default {
   include munin
 }
 
-node m1 inherits default {
-}
-
-node m2 inherits default {
-}
-
-node m3 inherits default {
+node m1, m2, m3 inherits default {
 }
