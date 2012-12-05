8c313f9c3b6f3ace454f3d1b22ff43786893508d
Y Sawa <yoshihirosawa@gmail.com>
2012-12-05 15:45:58 +0900

    add: services for emails

--- puppet/modules/email/manifests/dovecot.pp
+++ puppet/modules/email/manifests/dovecot.pp
@@ -3,4 +3,11 @@ class email::dovecot {
   package { 'dovecot-core':
     ensure => 'installed',
   }
+
+  service { 'dovecot':
+    require => [
+      Package['dovecot-core'],
+    ],
+    ensure => 'running',
+  }
 }
--- puppet/modules/email/manifests/postfix.pp
+++ puppet/modules/email/manifests/postfix.pp
@@ -3,4 +3,11 @@ class email::postfix {
   package { 'postfix':
     ensure => 'installed',
   }
+
+  service { 'postfix':
+    require => [
+      Package['postfix'],
+    ],
+    ensure => 'running',
+  }
 }
