b5f0c1b7c626d577d097feb56ea92ce15f3d0903
Y Sawa <yoshihirosawa@gmail.com>
2012-12-06 17:49:41 +0900

    add: my_option for ptetex3

--- puppet/modules/ptetex3/manifests/init.pp
+++ puppet/modules/ptetex3/manifests/init.pp
@@ -11,12 +11,28 @@ class ptetex3 {
     group   => root,
   }
 
+  file { '/tmp/puppet_ptetex3_my_option':
+    require => [
+      Package['build-essential'],
+    ],
+    ensure  => 'file',
+    source  => 'puppet:///modules/ptetex3/my_option',
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+  }
+
   exec { "/tmp/puppet_ptetex3_install.sh":
     require => [
-      File['/tmp/puppet_ptetex3_install.sh'],
+      File['/tmp/puppet_ptetex3_install.sh', '/tmp/puppet_ptetex3_my_option'],
     ],
     cwd       => '/tmp/',
-    timeout => 600,
+    timeout => 1200,
     # unless => '/bin/ls /usr/local/ptetex3/libexec/check_dhcp',
   }
+
+#  exec { 'append bin into PATH':
+#    command => 'cat "export PATH=/usr/local/teTeX/bin:$PATH" >> /etc/bashrc',
+#    unless => '/bin/true',
+#  }
 }
