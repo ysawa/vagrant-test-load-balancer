f6e01f7ba7632911d8f326e8f07a5e6c0b03c51b
Y Sawa <yoshihirosawa@gmail.com>
2012-12-06 17:07:25 +0900

    add: lv into essentials

--- puppet/modules/essentials/manifests/init.pp
+++ puppet/modules/essentials/manifests/init.pp
@@ -3,7 +3,8 @@ class essentials {
   $essentials = [
     'build-essential', 'automake', 'autoconf',
     'gettext', 'gettext-base',
-    'libxslt1.1', 'libxml2', 'libssl-dev', 'git-core', 'libffi-dev', 'libsqlite3-dev', 'libreadline6-dev', 'libgd2-xpm-dev'
+    'libxslt1.1', 'libxml2', 'libssl-dev', 'git-core', 'libffi-dev', 'libsqlite3-dev', 'libreadline6-dev', 'libgd2-xpm-dev',
+    'lv',
   ]
   package { $essentials:
     require => [
