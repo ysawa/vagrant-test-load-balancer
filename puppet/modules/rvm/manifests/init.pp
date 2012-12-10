b3d0ae53dccec209741a6dd20abfa04bbf0842b0
Y Sawa <yoshihirosawa@gmail.com>
2012-12-10 16:24:15 +0900

    modify: rvm installer

--- puppet/modules/rvm/manifests/init.pp
+++ puppet/modules/rvm/manifests/init.pp
@@ -1,31 +1,17 @@
 class rvm {
 
   define user ($username = $title) {
-    # curl -L https://get.rvm.io | bash -s stable --ruby
-    exec { "download rvm installer for $username":
+
+    file { "/home/$username/rvm-install.sh":
       require => [
         User[$username],
-        Package['curl'],
       ],
-      user => $username,
-      command => "curl -L https://get.rvm.io > rvm-install.sh ; chmod 755 rvm-install.sh",
-      path => [ "/bin", "/usr/bin", "/usr/local/bin", "/home/$username/bin" ],
-      cwd => "/home/$username",
-      unless => "ls /home/$username/.rvm",
+      ensure  => file,
+      mode    => 0755,
+      replace => no,
+      group => $username,
+      owner => $username,
+      source  => 'puppet:///modules/rvm/install.sh',
     }
-
-    # exec { "/home/$username/rvm-install.sh":
-    #   require => [
-    #     User[$username],
-    #     Exec["download rvm installer for $username"],
-    #     Package['bash', 'build-essential'],
-    #   ],
-    #   user => $username,
-    #   # command => "echo \"curl -L https://get.rvm.io | bash -s stable ; source /home/$username/.rvm/scripts/rvm\" | bash",
-    #   # command => "curl -L https://get.rvm.io | bash -s stable",
-    #   cwd => "/home/$username",
-    #   timeout => 600,
-    #   unless => "/bin/ls /home/$username/.rvm",
-    # }
   }
 }
