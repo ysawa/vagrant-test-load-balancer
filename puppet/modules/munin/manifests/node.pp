29daa3a185c6642c26d8c4196e9aa2a082a42ac0
Y Sawa <yoshihirosawa@gmail.com>
2012-12-11 18:34:13 +0900

    add: replacing munin-node.conf

--- puppet/modules/munin/manifests/node.pp
+++ puppet/modules/munin/manifests/node.pp
@@ -5,9 +5,37 @@ class munin::node {
     ensure => installed,
   }
 
-  service { 'munin-node':
+  file { '/etc/munin/munin-node.conf.puppet':
     require => [
       Package[$packages],
+    ],
+    ensure  => file,
+    content => template("munin/munin-node.conf.erb"),
+    mode    => 0644,
+    owner   => root,
+    group   => root,
+  }
+
+  exec { 'replace /etc/munin/munin-node.conf':
+    require => [
+      File['/etc/munin/munin-node.conf.puppet'],
+      Package[$packages],
+    ],
+    command => '/bin/mv /etc/munin/munin-node.conf.puppet /etc/munin/munin-node.conf',
+    unless => '/usr/bin/test `/bin/cat /etc/munin/munin-node.conf | /bin/grep -c "USED BY PUPPET$"` -ne 0',
+  }
+
+  exec { 'restart munin-node after installing munin-node':
+    require => [
+      Package[$packages],
+      Exec['replace /etc/munin/munin-node.conf'],
+    ],
+    command => '/usr/sbin/service munin-node reload ; /bin/true',
+  }
+
+  service { 'munin-node':
+    require => [
+      Exec['restart munin-node after installing munin-node'],
     ],
     ensure => running,
   }
