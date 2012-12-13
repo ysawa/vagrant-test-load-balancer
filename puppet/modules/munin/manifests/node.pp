07c10fa41c1df9fc4cab718488d01f5dc6fce608
Y Sawa <yoshihirosawa@gmail.com>
2012-12-13 15:13:34 +0900

    modify: plugin-conf.d

--- puppet/modules/munin/manifests/node.pp
+++ puppet/modules/munin/manifests/node.pp
@@ -25,10 +25,31 @@ class munin::node {
     unless => '/usr/bin/test `/bin/cat /etc/munin/munin-node.conf | /bin/grep -c "USED BY PUPPET$"` -ne 0',
   }
 
+  file { '/etc/munin/plugin-conf.d/munin-node.puppet':
+    require => [
+      Package[$packages],
+    ],
+    ensure  => file,
+    content => template("munin/plugin-conf.d/munin-node.erb"),
+    mode    => 0644,
+    owner   => root,
+    group   => root,
+  }
+
+  exec { 'replace /etc/munin/plugin-conf.d/munin-node':
+    require => [
+      File['/etc/munin/plugin-conf.d/munin-node.puppet'],
+      Package[$packages],
+    ],
+    command => '/bin/mv /etc/munin/plugin-conf.d/munin-node.puppet /etc/munin/plugin-conf.d/munin-node',
+    unless => '/usr/bin/test `/bin/cat /etc/munin/plugin-conf.d/munin-node | /bin/grep -c "USED BY PUPPET$"` -ne 0',
+  }
+
   exec { 'restart munin-node after installing munin-node':
     require => [
       Package[$packages],
       Exec['replace /etc/munin/munin-node.conf'],
+      Exec['replace /etc/munin/plugin-conf.d/munin-node'],
     ],
     command => '/usr/sbin/service munin-node reload ; /bin/true',
   }
